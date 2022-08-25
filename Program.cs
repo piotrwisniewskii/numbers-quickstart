9a76315febe95e65d4a1684e14d98fdab4aaec93
Piotr Wisniewski <piowisni@gmail.com>
2022-08-25 15:48:52 +0200

    Change1

---
+++ Program.cs
@@ -0,0 +1,63 @@
+// WorkWithIntegers();
+/*
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+OrderPrecedence();
+void OrderPrecedence()
+{
+    int a = 7;
+    int b = 4;
+    int c = 3;
+    int d = (a + b) / c;
+    int e = (a + b) % c;
+    Console.WriteLine(d);
+    Console.WriteLine(e);
+}
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The maximal value of int is {max} while the minimal value of int is {min}");
+
+int what = max + 3;
+Console.WriteLine(what);
+
+void WorkWithDoubles()
+
+{
+double a = 5;
+double b = 4;
+double c = 2;
+double d = (a + b)/ c;
+double e = 19;
+double f = 23;
+double g = 8;
+double h = (e + f) / g;
+Console.WriteLine(h);
+Console.WriteLine(d);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The maximal value of double is {max} and the minimal value of double is {min}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+}
+*/
+
